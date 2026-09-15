388ee62f8bce2b0e3ee93f6225ad8ffbb8f16241
Yohannes Alemu <alemuyohannes960@gmail.com>
2026-09-15 13:00:25 +0300

    Remove unused embedding and head module functions
    Removed unused functions from the module exports.

--- frozenllm/substrate/__init__.py
+++ frozenllm/substrate/__init__.py
@@ -19,9 +19,6 @@ from .architecture import (
     discover_layers,
     discover_layers_from_config,
     get_block_accessor,
-    get_embedding_module,
-    get_head_modules,
-    get_position_embedding_module,
     validate_interception_layers,
 )
 from .drift import compute_kl_drift
@@ -65,9 +62,6 @@ __all__ = [
     "discover_layers",
     "discover_layers_from_config",
     "get_block_accessor",
-    "get_embedding_module",
-    "get_head_modules",
-    "get_position_embedding_module",
     "validate_interception_layers",
     # Substrate & Execution
     "ForwardResult",
